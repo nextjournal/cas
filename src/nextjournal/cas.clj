0e13062c622972109beb8b51594a2254c41504f5
Dieter Komendera <dieter@komendera.com>
2021-08-23 20:08:42 +0200

    Allow setting custom content-type for uploads

--- src/nextjournal/cas.clj
+++ src/nextjournal/cas.clj
@@ -12,20 +12,29 @@
   (with-open [is (io/input-stream file)]
     (multihash/base58 (digest/sha2-512 is))))
 
-(defn upload! [config file]
-  (let [sha (base58-sha file)
-        target-path (str (:bucket config) "/data/" sha)
-        result @(process [(:exec-path config) "cp" file (str "gs://" target-path)]
-                         {:out :string})]
+(defn upload!
+  ([config file]
+   (upload! config file nil))
+  ([config file content-type]
+   (let [sha         (base58-sha file)
+         target-path (str (:bucket config) "/data/" sha)
+         args        (flatten [(:exec-path config)
+                               (when content-type
+                                 ["-h" (str "Content-Type:" content-type)])
+                               "cp" file (str "gs://" target-path)])
+         result      @(process args
+                               {:out :string})]
 
-    (if (zero? (:exit result))
-      (str "https://storage.googleapis.com/" target-path)
-      (throw (ex-info "error uploading to CAS"
-                      {:file file
-                       :config config
-                       :sha sha})))))
+     (if (zero? (:exit result))
+       (str "https://storage.googleapis.com/" target-path)
+       (throw (ex-info "error uploading to CAS"
+                       {:file   file
+                        :args   args
+                        :config config
+                        :sha    sha}))))))
 
 (comment
   (upload! config "examples/nextjournal.png")
+  (upload! config "examples/foo.edn" "application/edn")
   (digest/sha2-512 (io/input-stream  "examples/nextjournal.png"))
   (base58-sha "examples/nextjournal.png"))
