5e8079b720e347b9466db9c2282ce79a125a011c
Dieter Komendera <dieter@komendera.com>
2021-10-05 12:38:09 +0200

    First CAS upload implementation for GCS (#1)
    Defer to `gsutil` for auth & upload for now, but provide a clojure wrapper for it which can also be used from babashka tasks for example.
    
    On top of `gsutil` it provides some mime-type handling as well as the content addressing of the files before uploading them.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# cas
+# Nextjournal CAS
+
 Library for storing content-addressed blobs to cloud buckets.
+
+It currently only supports uploading to Google Cloud Storage.
+
+## Setup
+
+- Install `gsutil` and have it's `bin` directory on your `PATH`
+- use `gcloud auth login` and log in with a Google Cloud account which has write
+access to the `nextjournal-cas-eu` bucket
+
+## Use from the command line
+
+```
+$ bb upload examples/foo.edn -c application/edn
+$ bb upload examples/nextjournal.png
+```
+
+Looks like this:
+
+```
+$ bb upload examples/nextjournal.png           
+Copying file://examples/nextjournal.png [Content-Type=image/png]...
+/ [1 files][ 10.8 KiB/ 10.8 KiB]                                                
+Operation completed over 1 objects/10.8 KiB.                                     
+
+https://storage.googleapis.com/nextjournal-cas-eu/data/8Vsz72Uv7KB5DzU4UDFMmC1hU85JNgss6BkDS7zd3zyPiwZzJbNmZZHo3fe7VnhyD6B8GKLGwJrBaSVSPxwr7Wj4rH
+```
+
+## TODO
+
+- [x] add a nice way to call it from the command line
+- [ ] ability to override config via command line
+- [ ] abstraction for other cloud services
---
+++ src/nextjournal/cas.clj
@@ -0,0 +1,93 @@
+(ns nextjournal.cas
+  (:gen-class)
+  (:require [babashka.process :refer [process]]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure.string :as str]
+            [multihash.core :as multihash]
+            [multihash.digest :as digest]))
+
+(defn read-config
+  ([]
+   (read-config nil))
+  ([file]
+   (-> (or file "nextjournal.edn")
+       slurp
+       edn/read-string)))
+
+
+(defn base58-sha [file]
+  (with-open [is (io/input-stream file)]
+    (multihash/base58 (digest/sha2-512 is))))
+
+(defn mime-type [{:as _config :keys [mime-types]} file]
+  (some-> (filter (fn [[k _]] (str/ends-with? file k)) mime-types)
+          first
+          val))
+
+(defn upload!
+  "Uploads a `file` content addressed to cloud storage
+
+  For supported options in config see `nextjournal.cas/config`
+
+  For now assumes `gsutil` as the exec, readily set up and authenticated
+
+  If content-type is set, uses it as the Content-Type header. If it is not set,
+  let the underlying tooling figure out a content-type. This works for common types
+  like jpeg, png, etc."
+  ([config src-file]
+   (upload! config src-file nil))
+  ([config src-file content-type]
+   (assert (.exists (io/file src-file)) "File must exist")
+   (let [sha         (base58-sha src-file)
+         target-file (str (:target-path config) sha)
+         content-type (or content-type (mime-type config src-file))
+         args        (filter some?
+                             (flatten [(:exec-path config)
+                                       (when content-type
+                                         ["-h" (str "Content-Type:" content-type)])
+                                       "cp"
+                                       "-n" ;; No-clobber, don't upload an existing file
+                                       src-file
+                                       target-file]))
+         result      @(process args
+                               {:out :string
+                                :err :string})]
+
+     (if (zero? (:exit result))
+       {:url (str/replace-first target-file "gs://" "https://storage.googleapis.com/")
+        :out (:out result)
+        :err (:err result)}
+       (throw (ex-info "error uploading to CAS"
+                       {:src-file src-file
+                        :args     args
+                        :config   config
+                        :sha      sha
+                        :err      (:err result)}))))))
+
+(comment
+  (upload! (read-config "nextjournal.edn") "examples/nextjournal.png")
+  (upload! (read-config "nextjournal.edn") "examples/foo.edn")
+  (upload! (read-config "nextjournal.edn") "examples/foo.edn" "application/something-else")
+  (digest/sha2-512 (io/input-stream  "examples/nextjournal.png"))
+  (base58-sha "examples/nextjournal.png"))
+
+
+(def cli-options
+  [["-c" "--content-type CONTENT_TYPE" "the Content-Type header to use for the uploaded file"]
+   [nil "--config CONFIG" "the .edn configuration file to use (defaults to nextjournal.edn on the classpath)"]
+   ["-h" "--help"]])
+
+
+(defn -main [& args]
+  (let [opts (parse-opts args cli-options)]
+    (if (:help opts)
+      (println :summary opts)
+      (let [config (read-config (:config (:options opts)))
+            result (upload! config
+                            (first (:arguments opts))
+                            (:content-type (:options opts)))]
+        (println (:err result))
+        (println (:url result))
+        (shutdown-agents)))))
